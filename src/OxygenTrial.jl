61199fd489fe8b660cf42d7b7c89ec42fade9d9b
John Unwin <john@fingertip.co.nz>
2024-12-16 22:19:16 +1300

    Update Julia version to allow Revise

--- src/OxygenTrial.jl
+++ src/OxygenTrial.jl
@@ -1,5 +1,31 @@
 module OxygenTrial
 
+using Oxygen ; @oxidise
+#using HTTP
+
 # Write your package code here.
+get("/") do req::Request
+    return "Nothing to see here!"
+end
+
+get("/greet") do req::Request
+    return "hello Makara!"
+end
+
+get("/hi") do req::Request
+    return "Hi Northland!"
+end
+
+get("/addpath/{x}/{y}") do req::Request, x::Int, y::Int
+    return text("$x + $y = " * string(add(x, y)))
+end
+
+get("/addparms") do req::Request, x::Int, y::Int
+    return text("$x + $y = " * string(add(x, y)))
+end
+
+function add(a, b)
+    return a + b + 12
+end
 
 end
---
+++ web-server-revise.jl
@@ -0,0 +1,6 @@
+using Revise
+using Oxygen
+using OxygenTrial
+
+# start the web server
+OxygenTrial.serve(revise=:eager)
