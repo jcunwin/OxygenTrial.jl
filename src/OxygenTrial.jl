9f97592f698cdf8599dab3523f3900770ae81548
John Unwin <john@fingertip.co.nz>
2025-01-22 16:36:19 +1300

    Add favicon

--- src/OxygenTrial.jl
+++ src/OxygenTrial.jl
@@ -3,11 +3,20 @@ module OxygenTrial
 using Oxygen ; @oxidise
 using Dates
 
+staticfiles("assets", "assets")
+
 include("HealthCheck.jl")
 
 # Write your package code here.
 get("/") do req::Request
-    return "Nothing to see here!"
+    return html("""
+    <head>
+        <link rel="icon" href="/assets/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" href="/assets/favicon-32x32.png" sizes="32x32">
+    </head>
+    <h1>Oxygen</h1>
+    <p>Nothing to see here!</p>    
+    """)
 end
 
 get("/greet") do req::Request
